114d7537febe392d85a3550eba59c6ccbab3e9a7
Tommy Cahill <tcahill89@gmail.com>
2018-08-31 09:33:38 -0700

    added project description

--- main.py
+++ main.py
@@ -2,3 +2,5 @@
 # @author tcahill
 # @since 2018.08.30
 # @updated 2018.08.30
+# This project is intended to update a text file every second with the time,
+# which can then be used as a clock in OBS
